01315646e1af73f42a63865cd55ecc7306cea17d
wilamisaviz15 <wmaa@ic.ufal.br>
2022-01-25 17:09:30 -0300

    create frontend spa

---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,21 @@
+import { Course } from './../model/course';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { delay, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) {}
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      take(1),
+      // delay(500),
+      tap((courses) => {})
+    );
+  }
+}
