307fe37ac31781129a881aa472499175162e3b1c
wilamisaviz15 <wmaa@ic.ufal.br>
2022-01-25 21:41:08 -0300

    create backend application

---
+++ crud-spring/src/main/java/com/wilamis/controller/CourseController.java
@@ -0,0 +1,30 @@
+package com.wilamis.controller;
+
+import java.util.*;
+
+import com.wilamis.model.Course;
+import com.wilamis.repository.CourseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+// @AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
---
+++ crud-spring/src/main/java/com/wilamis/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.wilamis.repository;
+
+import com.wilamis.model.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
